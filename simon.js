5caed88ee69b4d6e5ea951c32d89bf8b591c4f9a
KAJALRAJAK <kajalrajak058@gmail.com>
2026-03-06 20:51:31 +0530

    Change event listener from keydown to click

--- simon.js
+++ simon.js
@@ -8,7 +8,7 @@ let level = 0;
 
 let h2 = document.querySelector("h2");
 
-document.addEventListener("keydown", function () {
+document.addEventListener("click", function () {
   if (!started) {
     console.log("game is started");
     started = true;
